efa7610253c3eac016ee60e0906220919ebfff1b
Marcos Crispino <mcrispino@MacBook-Pro-de-Marcos.local>
2015-05-28 13:13:03 -0300

    fixed: Not working for messages starting with 'Re:' or 'Fwd:'

--- content.js
+++ content.js
@@ -21,6 +21,21 @@ function hashHandler() {
 }
 var hashDetection = new hashHandler();
 
+function removeStringFromBegining(text, start) {
+	var len = start.length
+	if (text.substring(0, len) === start) {
+	    text = text.substring(len, text.length);
+	}
+	return text
+}
+
+function standarizedTitle(title) {
+	var aux = title;
+	aux = removeStringFromBegining(aux, "Re: ");
+	aux = removeStringFromBegining(aux, "Fwd: ");
+	return encodeURIComponent(aux);
+}
+
 var initialize = function() {
 	if(window.location.hash.indexOf("#inbox") !== -1) {
 		debug('gmail-issues add is running');
@@ -28,7 +43,7 @@ var initialize = function() {
 		var title = document.querySelectorAll(".hP")[0];
 		if (title != undefined) {
 			var aTag = document.createElement('a');
-    		aTag.setAttribute('href', 'https://issues.genexus.com/displaysearchissuesresults.aspx?' + encodeURIComponent(title.innerText));
+    		aTag.setAttribute('href', 'https://issues.genexus.com/displaysearchissuesresults.aspx?' + standarizedTitle(title.innerText));
     		aTag.setAttribute('style', 'font-size: small;');
     		aTag.innerHTML = "Search Issue";
 	    	debug(title.innerText);
